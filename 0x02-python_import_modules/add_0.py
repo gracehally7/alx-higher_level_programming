23fbe9409a247df89ebcf98d3e29ca8065c4fac8
gracehally7 <gracehally7@gmail.com>
2022-12-02 02:10:42 -0800

    add function

---
+++ 0x02-python_import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def(a,b):
+    """My addition function
+    Args:
+        a: first integer
+        b: second interger
+    Return:
+        the return value. a + b
+    """
+    return(a + b)
