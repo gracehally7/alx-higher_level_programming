c7eedeebd104c3f123aced229eb4381d77a3e8a7
gracehally7 <gracehally7@gmail.com>
2022-12-05 02:09:28 -0800

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/python3
+
+if __name__ = "__main__" :
+
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 1:
+        print("1 argument: ")
+   elif count == 0:
+       print("0 arguments.")
+   else:
+       print("{} arguments:" .format(count))
+
+  for i in range(count):
+      print("{}: {}".format(i+ 1, sys.argv[i+1])
+
