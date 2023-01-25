e6251640e744730fccd75d0f8b685d84eff1fd48
gracehally7 <99291785+gracehally7@users.noreply.github.com>
2023-01-25 13:33:09 +0100

    exceptions

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -2,20 +2,11 @@
 
 safe_print_list = __import__('0-safe_print_list').safe_print_list
 
-
-
-my_list = [1, 2, 37,4, 5]
-
-
+my_list = [1, 2, 3,4, 5]
 
 nb_print = safe_print_list(my_list, 2)
-
 print("nb_print: {:d}".format(nb_print))
-
 nb_print = safe_print_list(my_list, len(my_list))
-
 print("nb_print: {:d}".format(nb_print))
-
 nb_print = safe_print_list(my_list, len(my_list) + 2)
-
 print("nb_print: {:d}".format(nb_print))
